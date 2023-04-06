48caa050880a3a98c24bc124077fac99a221af62
Sharma <gossharma@deloitte.com>
2023-04-06 19:27:58 +0530

    New machine commit 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< HEAD
 
 const routes: Routes = [];
+=======
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path: "about", component: AboutusComponent},
+  {path:"contact", component: ContactusComponent}
+];
+>>>>>>> 3876234 (New machine commit)
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
